2d6ee76cf6b971e2c5fd5fa6233c2430b080a944
Anika438 <b24cm1009@iitj.ac.in>
2025-11-12 20:32:23 +0530

    Update ranker.h

---
+++ smart_autocomplete/include/ranker.h
@@ -0,0 +1,22 @@
+#ifndef RANKER_H
+#define RANKER_H
+
+#include <string>
+#include <vector>
+#include "freq_store.h"
+#include "graph.h"
+
+class Ranker {
+  private:
+      FreqStore* freqStore;
+      CooccurrenceGraph* graph;
+      std::string lastToken;
+  
+  public:
+      Ranker(FreqStore *fs,CooccurrenceGraph *g);
+      void setLastToken(const std::string &token);
+      double computeScore(const std::string &token);
+      std::vector<std::pair<std::string,double>> rankResults(const std::vector<std::string> &candidates,int k);
+};
+
+#endif
